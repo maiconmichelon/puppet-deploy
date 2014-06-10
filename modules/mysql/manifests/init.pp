fc2122cf2fd441d4309a70f3b215ae60cfe9ceb9
maiconmichelon <maicon.f.michelon@gmail.com>
2014-06-10 04:39:34 +0000

    Mysql funfando

--- modules/mysql/manifests/init.pp
+++ modules/mysql/manifests/init.pp
@@ -1,12 +1,26 @@
 class mysql {
+  package {"mysql-server":
+    ensure => "installed",
+    before => File["/etc/mysql/my.cnf"]
+  }
   file {"/etc/mysql/my.cnf":
-      owner => root,
-      group => root,
-      mode => 664,
-     # source => "/tmp/apache.cnf"
-     # source => "puppet:///modules/mysql/files/my.cnf";
-     # source => "/etc/puppet/modules/mysql/files/my.cnf"
-     # source => 'puppet:///modules/mysql/files/my.cnf'
-      source => "puppet:///modules/apache2/massiveapp.conf"
+    owner => root,
+    group => root,
+    mode => 664,
+    source => "puppet:///modules/mysql/my.cnf"
+  }
+  service {"mysql":
+    ensure => running,
+    subscribe => File["/etc/mysql/my.cnf"]
+  }
+  exec {
+    "mysql_password":
+      unless => "mysqladmin -uroot -proot status",
+      command => "mysqladmin -uroot password root",
+      require => Service["mysql"];
+    "massiveapp_db":
+      unless => "mysql -uroot -proot massiveapp_production",
+      command => "mysql -uroot -proot -e 'create database massiveapp_production'",
+      require => Exec["mysql_password"];
   }
 }
